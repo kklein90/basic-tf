c79dd84bd1bf433202448ffce34b389aa73b1e9e
kklein90 <kklein90@gmail.com>
2026-02-03 10:51:46 -0500

    simple tf outputs

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "name" {
+  description = "Name"
+  value       = "Kristopher Klein"
+}
+
+output "position_title" {
+  description = "Position Title"
+  value       = "Senior DevOps Engineer"
+}
